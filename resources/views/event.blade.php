76632fbbc75620f54196b33262afe39af108d7bb
rzlwbo1 <rizalwb2@gmail.com>
2022-05-19 11:03:35 +0700

    fix link user

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
   <h2>{{ $event->title }}</h2>
-  <p>By. <a href="#">{{ $event->user->name }}</a> dalam kategori <a href="/categories/{{ $event->category->slug }}">{{ $event->category->name }}</a></p>
+  <p>By. <a href="/users/{{ $event->user->username }}">{{ $event->user->name }}</a> dalam kategori <a href="/categories/{{ $event->category->slug }}">{{ $event->category->name }}</a></p>
   {{-- <p>{{ $event->body }}</p> --}}
 
   {!! $event->body !!}
