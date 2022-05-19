42092ccde1817773ac09956c37dfa5abd97187ee
rzlwbo1 <rizalwb2@gmail.com>
2022-05-19 10:17:20 +0700

    Seeder dengan factory >> event factory

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -25,7 +25,13 @@ class EventFactory extends Factory
         return [
             'category_id' => mt_rand(1, 3),
             'user_id' => mt_rand(1, 3),
-            'title' => $this->faker->text()
+            'title' => $this->faker->sentence(),
+            'slug' => $this->faker->slug(),
+            'priority' => mt_rand(0,5),
+            'excerpt' => $this->faker->paragraph(),
+            'body' => $this->faker->paragraph(3),
+            'publish_at' =>$this->faker->date(),
+            'time_at' => $this->faker->time()
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(3)->create();
+        User::factory()->count(3)->create();
 
         // User::create([
         //     "name" => "Rizal Wibowo",
@@ -47,41 +47,42 @@ class DatabaseSeeder extends Seeder
             "slug" => 'informasi'
         ]);
 
+        Event::factory()->count(10)->create();
 
-        Event::create([
-            "category_id" => 1,
-            'user_id' => 1,
-            'title' => "Pemilihan ketua RT",
-            'slug' => Str::slug('Pemilihan ketua RT', '-'),
-            'priority' => 1,
-            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed aliquam excepturi suscipit sint corporis assumenda cumque ullam saepe, ea repellat neque, minima ducimus magni animi omnis culpa tempore inventore iure',
-            "body" => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed aliquam excepturi suscipit sint corporis assumenda cumque ullam saepe, ea repellat neque, minima ducimus magni animi omnis culpa tempore inventore iure, quas quisquam provident ex! Sit, recusandae. Non expedita incidunt itaque amet earum odit libero, iusto impedit enim molestias eius porro magni sunt, nulla accusantium, voluptatem temporibus vero exercitationem dignissimos maiores soluta quis quisquam quae! Repudiandae eligendi aspernatur, laboriosam voluptas architecto molestias expedita quae fugiat sapiente voluptatem commodi dicta sunt eveniet neque quidem. Aperiam modi consequatur eaque dolorem labore molestias nam eius, perspiciatis quasi. Quis illo pariatur laudantium quidem autem consequatur.',
-            'publish_at' => date("Y-m-d"),
-            'time_at' => date('H:i:s')
-        ]);
+        // Event::create([
+        //     "category_id" => 1,
+        //     'user_id' => 1,
+        //     'title' => "Pemilihan ketua RT",
+        //     'slug' => Str::slug('Pemilihan ketua RT', '-'),
+        //     'priority' => 1,
+        //     'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed aliquam excepturi suscipit sint corporis assumenda cumque ullam saepe, ea repellat neque, minima ducimus magni animi omnis culpa tempore inventore iure',
+        //     "body" => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed aliquam excepturi suscipit sint corporis assumenda cumque ullam saepe, ea repellat neque, minima ducimus magni animi omnis culpa tempore inventore iure, quas quisquam provident ex! Sit, recusandae. Non expedita incidunt itaque amet earum odit libero, iusto impedit enim molestias eius porro magni sunt, nulla accusantium, voluptatem temporibus vero exercitationem dignissimos maiores soluta quis quisquam quae! Repudiandae eligendi aspernatur, laboriosam voluptas architecto molestias expedita quae fugiat sapiente voluptatem commodi dicta sunt eveniet neque quidem. Aperiam modi consequatur eaque dolorem labore molestias nam eius, perspiciatis quasi. Quis illo pariatur laudantium quidem autem consequatur.',
+        //     'publish_at' => date("Y-m-d"),
+        //     'time_at' => date('H:i:s')
+        // ]);
 
-        Event::create([
-            "category_id" => 3,
-            'user_id' => 1,
-            'title' => "Pembersihan Kantor RW",
-            'slug' => Str::slug('Pembersihan Kantor RW', '-'),
-            'priority' => 2,
-            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed aliquam excepturi suscipit sint corporis assumenda cumque ullam saepe, ea repellat neque, minima ducimus magni animi omnis culpa tempore inventore iure',
-            "body" => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed aliquam excepturi suscipit sint corporis assumenda cumque ullam saepe, ea repellat neque, minima ducimus magni animi omnis culpa tempore inventore iure, quas quisquam provident ex! Sit, recusandae. Non expedita incidunt itaque amet earum odit libero, iusto impedit enim molestias eius porro magni sunt, nulla accusantium, voluptatem temporibus vero exercitationem dignissimos maiores soluta quis quisquam quae! Repudiandae eligendi aspernatur, laboriosam voluptas architecto molestias expedita quae fugiat sapiente voluptatem commodi dicta sunt eveniet neque quidem. Aperiam modi consequatur eaque dolorem labore molestias nam eius, perspiciatis quasi. Quis illo pariatur laudantium quidem autem consequatur.',
-            'publish_at' => date("Y-m-d"),
-            'time_at' => date('H:i:s')
-        ]);
+        // Event::create([
+        //     "category_id" => 3,
+        //     'user_id' => 1,
+        //     'title' => "Pembersihan Kantor RW",
+        //     'slug' => Str::slug('Pembersihan Kantor RW', '-'),
+        //     'priority' => 2,
+        //     'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed aliquam excepturi suscipit sint corporis assumenda cumque ullam saepe, ea repellat neque, minima ducimus magni animi omnis culpa tempore inventore iure',
+        //     "body" => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed aliquam excepturi suscipit sint corporis assumenda cumque ullam saepe, ea repellat neque, minima ducimus magni animi omnis culpa tempore inventore iure, quas quisquam provident ex! Sit, recusandae. Non expedita incidunt itaque amet earum odit libero, iusto impedit enim molestias eius porro magni sunt, nulla accusantium, voluptatem temporibus vero exercitationem dignissimos maiores soluta quis quisquam quae! Repudiandae eligendi aspernatur, laboriosam voluptas architecto molestias expedita quae fugiat sapiente voluptatem commodi dicta sunt eveniet neque quidem. Aperiam modi consequatur eaque dolorem labore molestias nam eius, perspiciatis quasi. Quis illo pariatur laudantium quidem autem consequatur.',
+        //     'publish_at' => date("Y-m-d"),
+        //     'time_at' => date('H:i:s')
+        // ]);
 
-        Event::create([
-            "category_id" => 2,
-            'user_id' => 2,
-            'title' => "Gotong Royong warga",
-            'slug' => Str::slug('Gotong Royong warga', '-'),
-            'priority' => 2,
-            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed aliquam excepturi suscipit sint corporis assumenda cumque ullam saepe, ea repellat neque, minima ducimus magni animi omnis culpa tempore inventore iure',
-            "body" => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed aliquam excepturi suscipit sint corporis assumenda cumque ullam saepe, ea repellat neque, minima ducimus magni animi omnis culpa tempore inventore iure, quas quisquam provident ex! Sit, recusandae. Non expedita incidunt itaque amet earum odit libero, iusto impedit enim molestias eius porro magni sunt, nulla accusantium, voluptatem temporibus vero exercitationem dignissimos maiores soluta quis quisquam quae! Repudiandae eligendi aspernatur, laboriosam voluptas architecto molestias expedita quae fugiat sapiente voluptatem commodi dicta sunt eveniet neque quidem. Aperiam modi consequatur eaque dolorem labore molestias nam eius, perspiciatis quasi. Quis illo pariatur laudantium quidem autem consequatur.',
-            'publish_at' => date("Y-m-d"),
-            'time_at' => date('H:i:s')
-        ]);
+        // Event::create([
+        //     "category_id" => 2,
+        //     'user_id' => 2,
+        //     'title' => "Gotong Royong warga",
+        //     'slug' => Str::slug('Gotong Royong warga', '-'),
+        //     'priority' => 2,
+        //     'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed aliquam excepturi suscipit sint corporis assumenda cumque ullam saepe, ea repellat neque, minima ducimus magni animi omnis culpa tempore inventore iure',
+        //     "body" => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed aliquam excepturi suscipit sint corporis assumenda cumque ullam saepe, ea repellat neque, minima ducimus magni animi omnis culpa tempore inventore iure, quas quisquam provident ex! Sit, recusandae. Non expedita incidunt itaque amet earum odit libero, iusto impedit enim molestias eius porro magni sunt, nulla accusantium, voluptatem temporibus vero exercitationem dignissimos maiores soluta quis quisquam quae! Repudiandae eligendi aspernatur, laboriosam voluptas architecto molestias expedita quae fugiat sapiente voluptatem commodi dicta sunt eveniet neque quidem. Aperiam modi consequatur eaque dolorem labore molestias nam eius, perspiciatis quasi. Quis illo pariatur laudantium quidem autem consequatur.',
+        //     'publish_at' => date("Y-m-d"),
+        //     'time_at' => date('H:i:s')
+        // ]);
     }
 }
